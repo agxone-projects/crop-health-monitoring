883438f6dfcf9c9b8e7e9832d66ff37545bdb3bb
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2021-04-23 13:59:03 +0530

    model.py added

--- src/models/model.py
+++ src/models/model.py
@@ -1,7 +1,55 @@
-# -*- coding: utf-8 -*-
+import os
+
+import detectron2
+from detectron2.engine import DefaultTrainer, DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_test_loader
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+
+
+class CocoTrainer(DefaultTrainer):
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            os.makedirs("coco_eval", exist_ok=True)
+            output_folder = "coco_eval"
+        return COCOEvaluator(dataset_name, cfg, False, output_folder)
+
 
 class CornNetv2():
-    def __init__(self):
-        None
-    def compile(self):
-        None
+    def __init__(self, weights, n_classes, train_sets=()):
+        self.cfg = get_cfg()
+        self.cfg.merge_from_file(
+            "../../detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+        self.cfg.DATASETS.TRAIN = train_sets
+        self.cfg.DATASETS.TEST = ()
+        self.cfg.DATALOADER.NUM_WORKERS = 4
+        self.cfg.MODEL.WEIGHTS = weights
+        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = n_classes
+
+    def compile(self, n_iter, resume=False):
+        self.cfg.SOLVER.IMS_PER_BATCH = 5
+        self.cfg.SOLVER.BASE_LR = 0.0015
+        self.cfg.SOLVER.MAX_ITER = n_iter
+        self.cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
+
+        self.trainer = DefaultTrainer(self.cfg)
+        self.trainer.resume_or_load(resume=resume)
+
+    def fit(self, output_folder):
+        self.cfg.OUTPUT_DIR = output_folder
+        os.makedirs(self.cfg.OUTPUT_DIR, exist_ok=True)
+        self.trainer.train()
+
+    def evaluate(self, test_set):
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8
+        predictor = DefaultPredictor(self.cfg)
+        evaluator = COCOEvaluator(
+            test_set, self.cfg, False, output_dir="./output/")
+        val_loader = build_detection_test_loader(self.cfg, test_set)
+        inference_on_dataset(self.trainer.model, val_loader, evaluator)
+
+    def get_predict(self, score_thresh=0.7):
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = score_thresh
+        predictor = DefaultPredictor(self.cfg)
+        return predictor
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,1 +1,78 @@
-import jsonimport funcyfrom sklearn.model_selection import train_test_splitfrom detectron2.data.datasets import register_coco_instancesfrom detectron2.data import MetadataCatalog, DatasetCatalogdef save_coco(file, images, annotations, categories):  with open(file, 'wt', encoding='UTF-8') as coco:    json.dump({'images': images,       'annotations': annotations, 'categories': categories}, coco, indent=2, sort_keys=True)    def filter_annotations(annotations, images):  image_ids = funcy.lmap(lambda i: int(i['id']), images)  return funcy.lfilter(lambda a: int(a['image_id']) in image_ids, annotations)def split_coco_annotation(annotations, split_ratio, train_json = 'train_anno.json', test_json='test_anno.json', is_having = True):  with open(annotations, 'rt', encoding='UTF-8') as anno:    coco = json.load(anno)    images = coco['images']    annotations = coco['annotations']    categories = coco['categories']    images_with_annotations = funcy.lmap(lambda a: int(a['image_id']), annotations)    if is_having:        images = funcy.lremove(lambda i: i['id'] not in images_with_annotations, images)    x, y = train_test_split(images, train_size = split_ratio)    save_coco(train_json, x, filter_annotations(annotations, x), categories)    save_coco(test_json, y, filter_annotations(annotations, y), categories)    print("Saved {} entries in {} and {} in {}".format(len(x), train_json, len(y), test_json))if __name__ == '__main__':        #spliting the annotations    split_coco_annotation('../../data/interim/annotations.json', 0.9,                          '../../data/interim/train_annotations.json',                          '../../data/interim/test_annotations.json')        #registering the train and test datasets    register_coco_instances("corn_crops_train", {},                            '../../data/interim/train_annotations.json',                            '../../data/interim')    register_coco_instances("corn_crops_test", {},                            '../../data/interim/test_annotations.json',                             '../../data/interim')        #retrieving metadata and dicts    corn_crop_metadata = MetadataCatalog.get("corn_crops_train")    train_dataset_dicts = DatasetCatalog.get("corn_crops_train")    test_dataset_dicts = DatasetCatalog.get("corn_crops_test")                        
+import json
+import funcy
+from sklearn.model_selection import train_test_split
+
+import detectron2
+from detectron2.data.datasets import register_coco_instances
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+from model import CornNetv2
+
+
+def save_coco(file, images, annotations, categories):
+    with open(file, 'wt', encoding='UTF-8') as coco:
+        json.dump({'images': images,
+                   'annotations': annotations, 'categories': categories}, coco, indent=2, sort_keys=True)
+
+
+def filter_annotations(annotations, images):
+    image_ids = funcy.lmap(lambda i: int(i['id']), images)
+    return funcy.lfilter(lambda a: int(a['image_id']) in image_ids, annotations)
+
+
+def split_coco_annotation(annotations, split_ratio, train_json='train_anno.json', test_json='test_anno.json', is_having=True):
+    with open(annotations, 'rt', encoding='UTF-8') as anno:
+        coco = json.load(anno)
+        images = coco['images']
+        annotations = coco['annotations']
+        categories = coco['categories']
+
+        images_with_annotations = funcy.lmap(
+            lambda a: int(a['image_id']), annotations)
+
+        if is_having:
+            images = funcy.lremove(
+                lambda i: i['id'] not in images_with_annotations, images)
+
+        x, y = train_test_split(images, train_size=split_ratio)
+
+        save_coco(train_json, x, filter_annotations(
+            annotations, x), categories)
+        save_coco(test_json, y, filter_annotations(annotations, y), categories)
+
+        print("Saved {} entries in {} and {} in {}".format(
+            len(x), train_json, len(y), test_json))
+
+
+if __name__ == '__main__':
+
+    # spliting the annotations
+    split_coco_annotation('../../data/interim/annotations.json', 0.9,
+                          '../../data/interim/train_annotations.json',
+                          '../../data/interim/test_annotations.json')
+
+    # registering the train and test datasets
+    register_coco_instances("corn_crops_train", {},
+                            '../../data/interim/train_annotations.json',
+                            '../../data/interim')
+    register_coco_instances("corn_crops_test", {},
+                            '../../data/interim/test_annotations.json',
+                            '../../data/interim')
+
+    # retrieving metadata and dicts
+    corn_crop_metadata = MetadataCatalog.get("corn_crops_train")
+    train_dataset_dicts = DatasetCatalog.get("corn_crops_train")
+    test_dataset_dicts = DatasetCatalog.get("corn_crops_test")
+
+    model = CornNetv2(
+        weights="detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl",
+        n_classes=1,
+        train_sets=("corn_crops_train", ),)
+    model.compile(n_iter=1000, resume=True)
+
+    print('Training the Model\n')
+    output_dir = '../../models/cornnetv2'
+    model.fit(output_dir)
+
+    print('Evaluating using the Model')
+    model.evaluate("corn_crops_test")
