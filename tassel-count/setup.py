1b1e77caaf872b4c0c375e187ab388ddc97ee34b
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2021-04-25 01:32:10 +0530

    tassel count init

---
+++ tassel-count/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Counting tassels from Aerial images of corn crops with precision',
+    author='agxone-projects',
+    license='MIT',
+)
